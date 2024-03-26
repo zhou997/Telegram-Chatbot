bd0da2987f6f1ce4f8560ff65a13a91817629961
zhouweixuan <>
2024-03-26 17:32:07 +0800

    minimum integration of chatgpt and telegram bot

---
+++ ChatGPTHKBU.py
@@ -0,0 +1,25 @@
+import requests
+import os
+
+class ChatGPTHKBU:
+    def submit(self, message):
+        conversation = [{"role": "user", "content": message}]
+        url = (os.environ[
+            'CHATGPT_BASICURL']) + "/deployments/gpt-35-turbo/chat/completions/?api-version=2024-02-15-preview"
+        headers = {'Content-Type': 'application/json',
+                   'api-key': (os.environ['CHATGPT_ACCESS_TOKEN'])}
+        payload = {'messages': conversation}
+        response = requests.post(url, json=payload, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['choices'][0]['message']['content']
+        else:
+            return 'Error:', response
+
+
+if __name__ == '__main__':
+    ChatGPT_test = ChatGPTHKBU()
+    while True:
+        user_input = input("Typing anything to ChatGPT:\t")
+        response = ChatGPT_test.submit(user_input)
+        print(response)
---
+++ chatbot.py
@@ -0,0 +1,52 @@
+from telegram import Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          CallbackContext)
+import logging, os
+from ChatGPTHKBU import ChatGPTHKBU
+
+
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+
+def main():
+    updater = Updater(token=(os.environ['TELEGRAM_ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello))
+
+    # dispatcher for chatgpt
+    global chatgpt
+    chatgpt = ChatGPTHKBU()
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),
+                                     equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
+    # To start the bot:
+    updater.start_polling()
+    updater.idle()
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Helping you helping you.')
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    try:
+        reply_message = r"Good day, " + context.args[0] + r"!"
+        logging.info("Update: " + str(update))
+        logging.info("context: " + str(context))
+        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /hello <keyword>')
+
+
+if __name__ == '__main__':
+    main()
