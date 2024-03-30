ea70c0fb7e6d6a890b5031613412f4ef5978544f
Qiuzeyu <23462353@life.hkbu.edu.hk>
2024-03-31 00:25:28 +0800

    update the file

--- moviereviewtodatabase.py
+++ moviereviewtodatabase.py
@@ -1,8 +1,5 @@
 import pyodbc
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=ysql-zy.mysql.database.azure.com;UID=comp7940db;PWD=Cloud7940!,=')
-cursor = conn.cursor()
-
 cursor.execute('''CREATE TABLE IF NOT EXISTS reviews (
                     id INT PRIMARY KEY,
                     movie_name NVARCHAR(255),
