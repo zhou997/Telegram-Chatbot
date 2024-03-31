47d2cbff7a9681abe9eec3f3b9519319e34943d6
Qiuzeyu <23462353@life.hkbu.edu.hk>
2024-03-31 17:04:04 +0800

    the python script that can store the moviereviews to the database from the user

--- moviereviewtodatabase.py
+++ moviereviewtodatabase.py
@@ -1,4 +1,17 @@
+import os
 import pyodbc
+from dotenv import load_dotenv
+
+from mysqlconn import Database
+load_dotenv()
+
+azure_host = os.getenv("MYSQL_HOST")
+azure_database = os.getenv("chatbot")
+azure_username = os.getenv("MYSQL_USER")
+azure_password = os.getenv("MYSQL_PASSWORD")
+
+conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={azure_host};DATABASE={azure_database};UID={azure_username};PWD={azure_password}')
+cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS reviews (
                     id INT PRIMARY KEY,
@@ -6,11 +19,6 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS reviews (
                     rating INT,
                     review NVARCHAR(MAX)
                  )''')
-
-
-#movie_name = 'Inception'
-#rating = 5
-#review = 'A mind-bending masterpiece!' (the example of the reply function)
 
 cursor.execute("INSERT INTO reviews (movie_name, rating, review) VALUES (?, ?, ?)", (movie_name, rating, review))
 conn.commit()
